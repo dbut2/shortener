48bb06f744c9748d12bad6916e0e6dc468afcc62
dbut2 <dylanbutler@outlook.com.au>
2020-11-22 18:39:33 +1100

    tidy

--- internal/site/server.go
+++ internal/site/server.go
@@ -74,20 +74,6 @@ func (s *Shortener) shorten(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-type Server struct{}
-
-func (s *Server) Run(port string) {
-	r := chi.NewRouter()
-
-	r.Handle("/*", http.FileServer(http.Dir("./web")))
-	r.Handle("/static/*", http.FileServer(http.Dir("./web")))
-
-	err := http.ListenAndServe(":"+port, r)
-	if err != nil {
-		panic(err)
-	}
-}
-
 func respondError(w http.ResponseWriter, err error) {
 	respondJSON(w, http.StatusInternalServerError, err.Error())
 }
